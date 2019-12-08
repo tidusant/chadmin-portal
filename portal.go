f7256897cd8c5393b09e183e0f29f82b116dba9d
duy ha <duyhph@gmail.com>
2019-12-08 11:36:26 +0700

    re

--- portal.go
+++ portal.go
@@ -7,7 +7,7 @@ import (
 	"github.com/tidusant/c3m-common/log"
 	"github.com/tidusant/c3m-common/mycrypto"
 	rpsex "github.com/tidusant/chadmin-repo/session"
-	//"io"
+	//"io" re
 	"net"
 	"net/http"
 	"net/rpc"
